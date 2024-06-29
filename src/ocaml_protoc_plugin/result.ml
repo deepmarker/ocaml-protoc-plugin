a6eb81e61c15a639f9bb0c0d646a19fcfd1b2f00
Vincent Bernardoff <vb@luminar.eu.org>
2024-06-29 19:20:34 +0200

    result: simpler pp_error

--- src/ocaml_protoc_plugin/result.ml
+++ src/ocaml_protoc_plugin/result.ml
@@ -1,14 +1,16 @@
-(** This module provides result type and functions for compatibility
- * with OCaml 4.06 *)
+module ProtoField = Field
+open Core
 
 type error =
   [ `Premature_end_of_input
   | `Unknown_field_type of int
-  | `Wrong_field_type of string * Field.t
-  | `Illegal_value of string * Field.t
+  | `Wrong_field_type of string * ProtoField.t
+  | `Illegal_value of string * ProtoField.t
   | `Unknown_enum_value of int
   | `Oneof_missing
-  | `Required_field_missing ]
+  | `Required_field_missing ] [@@deriving sexp]
+
+let pp_error ppf t = Sexp.pp ppf (sexp_of_error t)
 
 type 'a t = ('a, error) result
 
@@ -26,46 +28,6 @@ let get ~msg = function
   | Ok v -> v
   | Error _ -> failwith msg
 
-let pp_error : Format.formatter -> [> error] -> unit = fun fmt -> function
-  | `Premature_end_of_input ->
-    Format.pp_print_string fmt
-      "`Premature_end_of_input"
-  | `Unknown_field_type x ->
-    (Format.fprintf fmt
-       "`Unknown_field_type (@[<hov>";
-     (Format.fprintf fmt "%d") x;
-     Format.fprintf fmt "@])")
-  | `Wrong_field_type x ->
-    (Format.fprintf fmt
-       "`Wrong_field_type (@[<hov>";
-     ((fun (a0, a1) ->
-         Format.fprintf fmt "(@[";
-         ((Format.fprintf fmt "%S") a0;
-          Format.fprintf fmt ",@ ";
-          (Field.pp fmt) a1);
-         Format.fprintf fmt "@])")) x;
-     Format.fprintf fmt "@])")
-  | `Illegal_value x ->
-    (Format.fprintf fmt
-       "`Illegal_value (@[<hov>";
-     ((fun (a0, a1) ->
-         Format.fprintf fmt "(@[";
-         ((Format.fprintf fmt "%S") a0;
-          Format.fprintf fmt ",@ ";
-          (Field.pp fmt) a1);
-         Format.fprintf fmt "@])")) x;
-     Format.fprintf fmt "@])")
-  | `Unknown_enum_value x ->
-    (Format.fprintf fmt
-       "`Unknown_enum_value (@[<hov>";
-     (Format.fprintf fmt "%d") x;
-     Format.fprintf fmt "@])")
-  | `Oneof_missing ->
-    Format.pp_print_string fmt "`Oneof_missing"
-  | `Required_field_missing ->
-    Format.pp_print_string fmt
-      "`Required_field_missing"
-let show_error : error -> string = Format.asprintf "%a" pp_error
 
 let pp pp fmt = function
   | Ok v -> Format.fprintf fmt "Ok %a" pp v
@@ -76,7 +38,7 @@ open Base
 
 let to_or_error = function
   | Ok x -> Ok x
-  | Error x -> Error (Error.of_string (show_error x))
+  | Error x -> Error (Error.of_lazy_sexp (lazy (sexp_of_error x)))
 ;;
 
 let ok_exn = function
